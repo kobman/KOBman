34f7f70348806592678cd01553610142df68bfac
EtricKombat <ericjoy1991@gmail.com>
2020-01-13 04:09:43 +0530

    Adding two folders and necessary files

--- bin/automata_zip_create.sh
+++ bin/automata_zip_create.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 echo "Before..."
-ls -latr ../kobman_zip_file.zip
-sudo zip -rj ../kobman_zip_file.zip ../src/main/bash/env/kobman-* ../src/main/bash/commands/kobman-*
+ls -latr ../releases/kobman-latest.zip
+
+sudo mv -i kobman-0.1.zip kobman-0.2.zip
+sudo mv -i kobman-latest.zip kobman-0.1.zip
+
+sudo zip -rj ../releases/kobman-latest.zip ../src/main/bash/env/kobman-* ../src/main/bash/commands/kobman-*
+
+
+
 echo "....after."
-ls -latr ../kobman_zip_file.zip
+ls -latr ../releases/kobman-latest.zip
 
 
