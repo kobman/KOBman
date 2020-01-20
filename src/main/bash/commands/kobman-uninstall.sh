61ce98f562a981ad04fd8510dabebba1689ca6a1
EtricKombat <ericjoy1991@gmail.com>
2020-01-21 02:25:08 +0530

    latest release

--- src/main/bash/commands/kobman-uninstall.sh
+++ src/main/bash/commands/kobman-uninstall.sh
@@ -1,41 +1,26 @@
 #!/usr/bin/env bash
 
-#
-#   Copyright 2017 Marco Vermeulen
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
 
 function __kob_uninstall {
 	
-local command qualifier three name_space
+local command deployment_type environment_type name_space
      	command=$COMMAND
-       	qualifier=$QUALIFIER
-       	three=$THREE
+       	deployment_type=$DEPLOYMENT_TYPE
+       	environment_type=$ENVIRONMENT_TYPE
        	name_space=$NAME_SPACE
   
   
 if [ "$command" = "uninstall" ]
 then
-        if [ "$QUALIFIER" = "tobvon" ]
+        if [ "$DEPLOYMENT_TYPE" = "tobvon" ]
         then
                 echo "uninstalling tobvon"
                 __kobman_tobvon_uninstall
-        elif [ "$QUALIFIER" = "tob" ]
+        elif [ "$DEPLOYMENT_TYPE" = "tob" ]
         then
                 echo "uninstalling tob"
                 __kobman_tob_uninstall
-        elif [ "$QUALIFIER" = "greenlight" ]
+        elif [ "$DEPLOYMENT_TYPE" = "greenlight" ]
         then
                 echo "uninstalling greenlight"
                 __kobman_greenlight_uninstall
