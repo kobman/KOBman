be2e78705fe8b4689086b5e3c2db6411ba711995
EtricKombat <ericjoy1991@gmail.com>
2020-01-30 01:28:28 +0530

    kobman-latest.zip updated with test memory upload codes

--- src/main/bash/commands/kobman-test.sh
+++ src/main/bash/commands/kobman-test.sh
@@ -4,6 +4,15 @@
 cd ${KOBMAN_DIR}/tmp
 sudo wget https://github.com/EtricKombat/KOBman/blob/master/releases/kobman-test.zip
 unzip kobman-test.zip
-  
- 
+
+figlet Upload
+figlet Test Functions  
+OLD_IFS="$IFS"
+IFS=$'\n'
+scripts=($(find "${KOBMAN_DIR}/tests/commands" "${KOBMAN_DIR}/tests/envs" -type f -name 'test-kob-*'))
+for f in "${scripts[@]}"; do
+        source "$f"
+done
+IFS="$OLD_IFS"
+unset scripts f 
 
