626e196ec19b0d4790995044d48e82a2ca08da56
EtricKombat <ericjoy1991@gmail.com>
2020-01-30 09:22:16 +0530

    greelight file adding ...version printing of deployment type

--- src/main/bash/commands/kobman-version.sh
+++ src/main/bash/commands/kobman-version.sh
@@ -5,7 +5,7 @@ function __kob_version {
 
                                                                    
 sudo figlet KOB - Version -f small
-
+echo $DEPLOYMENT _TYPE 
 __kobman_environment_version "$DEPLOYMENT _TYPE" 
 #	__kobman_echo_yellow ${KOBMAN_VERSION_0.01}
 
