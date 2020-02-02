d76cc30a2b2eed02e10b404d8d6f3221b7db9144
EtricKombat <33585301+EtricKombat@users.noreply.github.com>
2020-02-02 23:25:20 +0530

    kob connect figlet arrangement

--- src/main/bash/env/kobman-kobconnect.sh
+++ src/main/bash/env/kobman-kobconnect.sh
@@ -5,9 +5,9 @@ function __kobman_install_kobconnect
 {
 
 		kobman_namespace="$1"
-		
-		sudo figlet KOB-Connect -f small
 		sudo figlet Building -f small
+		sudo figlet KOB-Connect -f small
+		sudo figlet from -f small
 		sudo figlet ${kobman_namespace} -f small		
 		cd ${KOBMAN_CANDIDATES_DIR}
 		sudo git clone https://github.com/${kobman_namespace}/KOBConnect.git
@@ -15,9 +15,10 @@ function __kobman_install_kobconnect
 
 function __kobman_uninstall_kobconnect
 {
-
+	sudo figlet Uninstalling -f small
 	sudo figlet KOB-Connect -f small
-	sudo figlet Removing -f small
+	sudo figlet from -f small
+	sudo figlet ${kobman_namespace} -f small
 	cd ${KOBMAN_CANDIDATES_DIR}
 	sudo rm -rf KOBConnect/
 
