90b849f361d9a2d1b65179b503f799df0737b187
EtricKombat <33585301+EtricKombat@users.noreply.github.com>
2020-02-02 23:33:39 +0530

    re-arranged figlet for kobvon

--- src/main/bash/env/kobman-kobvon.sh
+++ src/main/bash/env/kobman-kobvon.sh
@@ -6,8 +6,10 @@ function __kobman_install_kobvon
 
 		kobman_namespace="$1"
  	
-		sudo figlet KOB-VON -f small
+		
 		sudo figlet Building -f small
+		sudo figlet KOBVON -f small
+		sudo figlet from -f small
 		sudo figlet ${kobman_namespace} -f small  
 		cd ${KOBMAN_CANDIDATES_DIR}
                 sudo git clone https://github.com/${kobman_namespace}/von-network.git
@@ -20,8 +22,10 @@ function __kobman_start_kobvon
 {
 
 
- 	sudo figlet KOB-VON -f small
-	sudo figlet Starting -f small
+ 	sudo figlet Starting -f small
+	sudo figlet KOBVON -f small
+	sudo figlet from -f small
+	sudo figlet ${kobman_namespace} -f small  
 	cd ${KOBMAN_CANDIDATES_DIR}
         sudo von-network/manage start
 }
