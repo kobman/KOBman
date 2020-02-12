8a73400b33edd5e91257448dde9c2eac207f4d4d
EtricKombat <ericjoy1991@gmail.com>
2020-02-12 18:12:31 +0530

    remaking automata scripts

--- bin/automata_new_env.sh
+++ bin/automata_new_env.sh
@@ -3,5 +3,13 @@
 sudo apt install figlet -y
 sudo figlet Fresh -f small 
 sudo figlet Environment -f small
+mkdir -p /usr/home/kobman
+cd /usr/home/kobman
 sudo git clone https://github.com/EtricKombat/KOBman.git
+sudo git clone https://github.com/EtricKombat/TheOrgBook
+sudo git clone https://github.com/EtricKombat/greenlight
+sudo git clone https://github.com/EtricKombat/von-network
+
+
+
 curl -L https://raw.githubusercontent.com/EtricKombat/KOBman/master/get.kobman.io | bash
