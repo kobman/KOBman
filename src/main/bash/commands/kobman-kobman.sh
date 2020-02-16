6394b0d825a28ae7ec6d58a2a252d26c75125678
EtricKombat <ericjoy1991@gmail.com>
2020-02-16 15:19:11 +0530

    kobman dev script adding

---
+++ src/main/bash/commands/kobman-kobman.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+sudo cd ~
+sudo mkdir -p ${Repo_name}
+sudo mkdir -p test/ dependency/
+sudo git clone https://github.com/EtricKombat/${Repo_name}
+# sudo cd ${Repo_name}
+# sudo mv . ../src
+
+ 
