ba14315acb07c7bfa461bf135101defee845947b
EtricKombat <ericjoy1991@gmail.com>
2020-02-16 22:57:20 +0530

    kobman-kobman dev script sudo removal

--- src/main/bash/env/kobman-kobman.sh
+++ src/main/bash/env/kobman-kobman.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-
+Repo_name=${3:-KOBman}
 __kobman_echo_red "kobman dev script initiating"
-sudo cd ~
+cd ~
 sudo mkdir -p ${Repo_name}
+cd ${Repo_name}
 sudo mkdir -p test/ dependency/
 sudo git clone https://github.com/EtricKombat/${Repo_name}
-# sudo cd ${Repo_name}
 # sudo mv . ../src
 
  
