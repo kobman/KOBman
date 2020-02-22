a11e50da91864d9878289691359097ff4354ebd3
EtricKombat <ericjoy1991@gmail.com>
2020-02-22 20:56:31 +0530

    date : feb-22-2020 time:20:56 ... fix on broken kobman-latest.zip file

--- bin/automata_local_repo_releases_zip.sh
+++ bin/automata_local_repo_releases_zip.sh
@@ -2,15 +2,21 @@
 
 echo "Before..."
 ls -latr ../releases/kobman-latest.zip
-cd ~/KOBman/releases/
-sudo mv -i kobman-0.1.zip kobman-0.2.zip
-sudo mv -i kobman-latest.zip kobman-0.1.zip
 
-sudo zip -rj ../releases/kobman-latest.zip ../src/main/bash/env/kobman-* ../src/main/bash/commands/kobman-*
+
+cd ~/KOBman/
+
+#sudo zip -rj ../releases/kobman-latest.zip ../src/main/bash/env/kobman-* ../src/main/bash/commands/kobman-*
+sudo zip -rj releases/kobman-latest.zip src/main/bash/env/kobman-* src/main/bash/commands/kobman-*
+
+#cd ~/KOBman/releases/
+#sudo mv -i kobman-0.1.zip kobman-0.2.zip
+#sudo mv -i kobman-latest.zip kobman-0.1.zip
+
 
 
 
 echo "....after."
-ls -latr ../releases/kobman-latest.zip
+ls -latr releases/kobman-latest.zip
 
 
