c74609bb69cd316ece5a52d3dd0f0a16ab2cab6f
EtricKombat <ericjoy1991@gmail.com>
2020-02-27 02:47:33 +0530

    kobman-kobman.sh script directory setup

--- src/main/bash/env/kobman-kobman.sh
+++ src/main/bash/env/kobman-kobman.sh
@@ -5,12 +5,11 @@ function __kob_kobman {
 
 
 Repo_name=${3:-KOBman}
-__kobman_echo_red "kobman dev script initiating"
+__kobman_echo_red "KOBman development environment setting up at /usr/home/kobman_dev_dir "
 cd ~
-sudo mkdir -p ${Repo_name}
-cd ${Repo_name}
+sudo mkdir -p /usr/home/${Repo_name}_dev_dir_Time:"$(date +%M)"
+cd ${Repo_name}_dev_dir_Time:"$(date +%M)"
 sudo mkdir -p test/ dependency/
 sudo git clone https://github.com/EtricKombat/${Repo_name}
-# sudo mv . ../src
 
 } 
