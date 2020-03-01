57a4c31411562e30157c995d2db168c0b5663255
EtricKombat <ericjoy1991@gmail.com>
2020-03-02 01:36:04 +0530

    status command fix

--- src/main/bash/commands/kobman-status.sh
+++ src/main/bash/commands/kobman-status.sh
@@ -5,6 +5,9 @@ function __kob_status {
 
 sudo rm ${KOBMAN_CANDIDATES_DIR}/*.tar.gz 2> /dev/null
 sudo rm ${KOBMAN_CANDIDATES_DIR}/source-to-image-v1.1.14-874754de-linux-amd64.tar.gz.* 2> /dev/null
+
+__kobman_echo_cyan "=========STATUS========="
+
 sudo figlet status -f small
-sudo ls ${KOBMAN_CANDIDATES_DIR}
+sudo tree DevDir_* ${KOBMAN_CANDIDATES_DIR}
 }
