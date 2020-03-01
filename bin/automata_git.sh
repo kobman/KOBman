4e968c51d0b07abbbc24c2726b920d72eef11248
EtricKombat <ericjoy1991@gmail.com>
2020-03-01 23:26:40 +0530

    removing KOBMAN_DEV environement varialble commit on: Mar  1, 2020 23:26:40

--- bin/automata_git.sh
+++ bin/automata_git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo git pull origin master
-sudo git add .
+git pull origin master
+git add .
 read -p "please insert your commit message" msg
-sudo git commit -m "$msg commit on: $(date)"
-sudo git push origin master
+git commit -m "$msg commit on: $(date)"
+git push origin master
