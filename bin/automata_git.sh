47828e53797536b119612fd57528dc3f2e760334
EtricKombat <ericjoy1991@gmail.com>
2020-03-03 01:10:16 +0530

    automata_git.sh final commit on: Tue Mar  3 01:10:16 IST 2020

--- bin/automata_git.sh
+++ bin/automata_git.sh
@@ -2,6 +2,6 @@
 cd ../
 sudo git pull origin master
 sudo git add .
-read -p "Enter your commit message : " msg
+read -p "Enter your commited message : " msg
 sudo git commit -m "${msg} commit on: $(date)"
 sudo git push origin master
