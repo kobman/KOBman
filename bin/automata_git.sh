697a69e076fc6acbb235e3fcd1f86acfe0cdbf74
EtricKombat <ericjoy1991@gmail.com>
2020-03-03 01:06:11 +0530

    testing automata git script commit on: Tue Mar  3 01:06:11 IST 2020

--- bin/automata_git.sh
+++ bin/automata_git.sh
@@ -2,6 +2,6 @@
 cd ../
 sudo git pull origin master
 sudo git add .
-sudo read -p "please insert your commit message" msg
+read -p "please insert your commit message" msg
 sudo git commit -m "$msg commit on: $(date)"
 sudo git push origin master
