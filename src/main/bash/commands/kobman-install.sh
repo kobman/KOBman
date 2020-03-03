4146c64605c1ac40a6185dd8648e7b764399e32e
EtricKombat <ericjoy1991@gmail.com>
2020-03-03 16:07:11 +0530

    docker issue fix 2 commit on: Tue Mar  3 16:07:11 IST 2020

--- src/main/bash/commands/kobman-install.sh
+++ src/main/bash/commands/kobman-install.sh
@@ -362,7 +362,6 @@ function __kobman_docker_install {
         sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
 	__kobman_echo_yellow "Installing Docker-Engine"
 	sudo apt-get update -y
-        sudo apt-get install docker-ce docker-ce-cli containerd.io -y
         sudo docker run hello-world
 	__kobman_echo_yellow "Installing Docker-Compose"
 	sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
