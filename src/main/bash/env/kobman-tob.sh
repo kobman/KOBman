fa85b170580ace0a895f66ff36b5b386629b046a
EtricKombat <ericjoy1991@gmail.com>
2020-03-04 17:17:29 +0530

    fixing missing operand issue commit on: Wed Mar  4 17:17:29 IST 2020

--- src/main/bash/env/kobman-tob.sh
+++ src/main/bash/env/kobman-tob.sh
@@ -6,8 +6,8 @@ function __kobman_development_tob_dir {
 	sudo mkdir -p DevDir_TOB
 	cd DevDir_TOB
 	export KOBMAN_TOB_DEV_DIR=$PWD
-	sudo chmod 777 ${KOBMAN_TOB_DEV_DIR}
-	cd ${KOBMAN_TOB_DEV_DIR}
+	sudo chmod 777 $KOBMAN_TOB_DEV_DIR
+	cd $KOBMAN_TOB_DEV_DIR
 	sudo mkdir -p test/ dependency/
 
                                                             
@@ -42,15 +42,13 @@ function __kobman_uninstall_tob
 {
 #	sudo chmod 777 KOBMAN_CANDIDATES_DIR
  	__kobman_echo_cyan "TheOrgBook - Uninstalling..."	
-	sudo chmod 777 $KOBMAN_TOB_DEV_DIR
-	cd ${KOBMAN_TOB_DEV_DIR}
+	sudo chmod 777 KOBMAN_TOB_DEV_DIR
+	cd $KOBMAN_TOB_DEV_DIR
 	sudo TheOrgBook/docker/manage rm 2> /dev/null	
 	sudo rm ${KOBMAN_TOB_DEV_DIR}/source-* 2> /dev/null	
 	sudo rm -rf TheOrgBook/ 2> /dev/null 	
 	sudo rm -rf /usr/local/bin/s2i /usr/local/bin/sti TheOrgBook/ 2> /dev/null	
 
-        #cd ~
-        #sudo rm -rf DevDir_TOB 2> /dev/null
 	cd ~
 	sudo rm -rf $KOBMAN_TOB_DEV_DIR 2> /dev/null
 	cd ~
