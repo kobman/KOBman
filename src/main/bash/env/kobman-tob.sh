fb1d401a342e38e2e8db6995153dce6c2b3dee5d
EtricKombat <ericjoy1991@gmail.com>
2020-03-21 13:09:02 +0530

    kobman-tob.sh uninstall fun

--- src/main/bash/env/kobman-tob.sh
+++ src/main/bash/env/kobman-tob.sh
@@ -42,8 +42,8 @@ function __kobman_uninstall_tob
 {
 #	sudo chmod 777 KOBMAN_CANDIDATES_DIR
  	__kobman_echo_cyan "TheOrgBook - Uninstalling..."	
-	sudo chmod 777 $KOBMAN_TOB_DEV_DIR
-	cd $KOBMAN_TOB_DEV_DIR
+#	sudo chmod 777 ${KOBMAN_TOB_DEV_DIR}
+	cd ${KOBMAN_TOB_DEV_DIR}
 	sudo TheOrgBook/docker/manage rm 2> /dev/null	
 	sudo rm ${KOBMAN_TOB_DEV_DIR}/source-* 2> /dev/null	
 	sudo rm -rf TheOrgBook/ 2> /dev/null 	
