a73ebe57f22126bbcb296105731d3e8adcd9c8a3
EtricKombat <ericjoy1991@gmail.com>
2020-04-03 09:23:22 +0530

    reflecting src/main/awk/git-describe script in kobman-latest.zip commit on: Fri Apr  3 09:23:22 IST 2020

--- bin/automata_local_repo_releases_zip.sh
+++ bin/automata_local_repo_releases_zip.sh
@@ -6,14 +6,7 @@ ls -latr ../releases/kobman-latest.zip
 
 cd ~/KOBman/
 
-#sudo zip -rj ../releases/kobman-latest.zip ../src/main/bash/env/kobman-* ../src/main/bash/commands/kobman-*
-sudo zip -rj releases/kobman-latest.zip src/main/bash/env/kobman-* src/main/bash/commands/kobman-*
-
-#cd ~/KOBman/releases/
-#sudo mv -i kobman-0.1.zip kobman-0.2.zip
-#sudo mv -i kobman-latest.zip kobman-0.1.zip
-
-
+sudo zip -rj releases/kobman-latest.zip src/main/bash/env/kobman-* src/main/bash/commands/kobman-* src/main/awk/git-describe
 
 
 echo "....after."
