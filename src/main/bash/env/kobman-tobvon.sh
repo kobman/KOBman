ca1baba7f8ce52f26e80406d50da359259ab7552
EtricKombat <ericjoy1991@gmail.com>
2020-04-07 15:55:15 +0530

    fixing tobvon vertsion commit on: Tue Apr  7 15:55:15 IST 2020

--- src/main/bash/env/kobman-tobvon.sh
+++ src/main/bash/env/kobman-tobvon.sh
@@ -49,11 +49,10 @@ function __kobman_uninstall_tobvon
 function __kobman_version_tobvon
 {
 
-	if [ -d "${KOBMAN_TOBVON_DEV_DIR" ]
+	if [ -d "${KOBMAN_TOBVON_DEV_DIR}" ]
 	then 
 		kobman_namespace="$1"
-		cd "${KOBMAN_VON_DEV_DIR}" 
-		echo "${KOBMAN_VON_DEV_DIR}" 
+		cd ${KOBMAN_VON_DEV_DIR} 
 		cd von-network/	
 		git show-ref --tag | grep -o v0.0.*
 		cd ~
