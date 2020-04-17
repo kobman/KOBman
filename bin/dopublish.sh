d447d34b39890a9f95e89831f74f58575fe87938
Arun Suresh <arunsureshampadath@gmail.com>
2020-04-17 09:31:13 +0530

    Files added

---
+++ bin/dopublish.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+KOB_REL_VERSION=$1
+KOB_ARCHIVE_DOWLOAD_REPO="KOBman_target_repo"
+BRANCH="master"
+KOB_NAMESPACE="asa1997"
+
+#Checkout latest tag
+git checkout tags/$KOB_REL_VERSION -b $KOB_REL_VERSION-branch
+
+if [ -d $KOB_ARCHIVE_DOWLOAD_REPO ]
+    then
+        rm -rf $KOB_ARCHIVE_DOWLOAD_REPO
+    else
+        echo "cloning $KOB_ARCHIVE_DOWLOAD_REPO"
+        git clone https://github.com/$KOB_NAMESPACE/$KOB_ARCHIVE_DOWLOAD_REPO
+        mkdir $KOB_ARCHIVE_DOWLOAD_REPO/dist
+fi 
+
+echo "moving necessary files to target"
+tar -cvf kobman_latest.tar ~/dev_branch/KOBman/src/ ~/dev_branch/KOBman/bin/
+cp kobman_latest.tar kobman-$KOB_REL_VERSION.tar
+mv *.tar $KOB_ARCHIVE_DOWLOAD_REPO/dist 
+mv ~/dev_branch/KOBman/scripts/get.kobman.io ~/dev_branch/KOBman/$KOB_ARCHIVE_DOWLOAD_REPO/dist
+
+echo "moving into $KOB_ARCHIVE_DOWLOAD_REPO"
+cd $KOB_ARCHIVE_DOWLOAD_REPO
+
+echo "saving changes and pushing"
+git add .
+# sudo git add KOBman-installer.sh
+git commit -m "Update version of $BRANCH to $KOB_ARCHIVE_DOWLOAD_REPO"
+# echo "pushing to release master"
+git push origin master -f
---
+++ bin/release.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+KOB_VERSION="$1"
+
+BRANCH="production"
+
+KOB_ARCHIVE_DOWLOAD_REPO="KOBman_website"
+
+KOBMAN_NAMESPACE="asa1997"
+
+#sanityls
+# if [[ -z "$VERSION" ]]; then
+# 	echo "Usage: release.sh <version>"
+# 	exit 0
+# fi
+
+echo "$KOB_VERSION"
+#prepare branch
+# echo "prepare branch"
+# #git fetch
+# git checkout master
+# git branch -D "$BRANCH"
+# git checkout -b "$BRANCH"
+
+#copy the tmpl file to /scripts
+echo "copying tmpl file to scripts"
+cp ~/dev_branch/KOBman/scripts/tmpl/get.kobman.io.tmpl ~/dev_branch/KOBman/scripts/
+
+mv ~/dev_branch/KOBman/scripts/get.kobman.io.tmpl ~/dev_branch/KOBman/scripts/get.kobman.io
+
+#replacing variables with actual values
+echo "updating variables"
+sed -i "s/@KOB_VERSION@/$KOB_VERSION/g" ~/dev_branch/KOBman/scripts/get.kobman.io
+sed -i "s/@KOB_ARCHIVE_DOWLOAD_REPO@/$KOB_ARCHIVE_DOWLOAD_REPO/g" ~/dev_branch/KOBman/scripts/get.kobman.io
+sed -i "s/@KOB_NAMESPACE@/$KOBMAN_NAMESPACE/g" ~/dev_branch/KOBman/scripts/get.kobman.io
+
+#Tagging 
+echo "tagging"
+git tag -a "$KOB_VERSION" -m "Releasing version $KOB_VERSION"
+echo "pushing version"
+git push origin $KOB_VERSION
+
+#pushing get.kobman.io to repo
+sudo git add ~/dev_branch/KOBman/scripts/get.kobman.io
+sudo git commit -m "variables updated"
+sudo git push dev Dev
+
