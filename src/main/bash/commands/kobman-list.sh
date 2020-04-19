8ab8355d40eb631b00c3d2518e105b8bf75a1195
EtricKombat <33585301+EtricKombat@users.noreply.github.com>
2020-04-20 01:29:14 +0530

    Update kobman-list.sh

--- src/main/bash/commands/kobman-list.sh
+++ src/main/bash/commands/kobman-list.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function __kob_list {
-curl -L "https://raw.githubusercontent.com/EtricKombat/KOBman/master/list" | less
+curl -L "https://raw.githubusercontent.com/${KOBMAN_NAMESPACE}/KOBman/master/dist/list" | less
 
 
 }
