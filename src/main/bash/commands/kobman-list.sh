29d218e65415e50d3116acb768daa4867dc83f52
EtricKombat <33585301+EtricKombat@users.noreply.github.com>
2020-04-20 10:10:43 +0530

    Update kobman-list.sh

--- src/main/bash/commands/kobman-list.sh
+++ src/main/bash/commands/kobman-list.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function __kob_list {
+echo "printing namespace"
 echo $KOBMAN_NAMESPACE
 curl -L "https://raw.githubusercontent.com/${KOBMAN_NAMESPACE}/KOBman/master/dist/list" | less
 
