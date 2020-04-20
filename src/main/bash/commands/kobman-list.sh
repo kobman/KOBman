c7f1cf99372abfe21eb3e29b82293d066e64e881
EtricKombat <33585301+EtricKombat@users.noreply.github.com>
2020-04-20 11:12:47 +0530

    fixing export for environment variables

--- src/main/bash/commands/kobman-list.sh
+++ src/main/bash/commands/kobman-list.sh
@@ -2,5 +2,5 @@
 
 function __kob_list {
 
-curl -L "${KOBMAN_SERVICE}/${KOBMAN_NAMESPACE}/KOBman/master/dist/list" | less
+curl -L "$(KOBMAN_SERVICE)/$(KOBMAN_NAMESPACE)/KOBman/master/dist/list" | less
 }
