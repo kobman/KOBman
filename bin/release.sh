cf9a2427f95a8d1f9adfa07ee47e31ed7ddd6b4c
Arun Suresh <arunsureshampadath@gmail.com>
2020-05-04 09:57:41 +0530

    Readme added in /tmpl

--- bin/release.sh
+++ bin/release.sh
@@ -28,14 +28,14 @@ mv $KOBMAN_DIR/scripts/README.md.tmpl $KOBMAN_DIR/scripts/README.md
 sed -i "s/@KOB_VERSION@/$kob_version/g" $KOBMAN_DIR/scripts/get.kobman.io
 sed -i "s/@KOB_ARCHIVE_DOWNLOAD_REPO@/$KOB_ARCHIVE_DOWNLOAD_REPO/g" $KOBMAN_DIR/scripts/get.kobman.io
 sed -i "s/@KOB_NAMESPACE@/$KOB_NAMESPACE/g" $KOBMAN_DIR/scripts/get.kobman.io
-sed -i "s/@KOB_ARCHIVE_DOWNLOAD_REPO@/$KOB_ARCHIVE_DOWNLOAD_REPO/g" $KOBMAN_DIR/README.md
-sed -i "s/@KOB_NAMESPACE@/$KOB_NAMESPACE/g" $KOBMAN_DIR/README.md
+sed -i "s/@KOB_ARCHIVE_DOWNLOAD_REPO@/$KOB_ARCHIVE_DOWNLOAD_REPO/g" $KOBMAN_DIR/scripts/README.md
+sed -i "s/@KOB_NAMESPACE@/$KOB_NAMESPACE/g" $KOBMAN_DIR/scripts/README.md
 
 sudo git add .
 sudo git commit -m "Variables replaced with originals"
 
 #Tagging and pushing 
-git tag -a "$kob_version" -m "Releasing version $kob_version"
+sudo git tag -a "$kob_version" -m "Releasing version $kob_version"
 git push origin $kob_version
 
 git checkout -b $branch
