56d8d933f914a26204e9b8b782d1531f56d72c19
Arun Suresh <arunsureshampadath@gmail.com>
2020-05-05 13:14:07 +0530

    Variables replaced with originals

--- bin/dopublish.sh
+++ bin/dopublish.sh
@@ -5,36 +5,52 @@ kob_rel_version=$1
 kob_rel_stage_area=~/tmp
 #kob_namespace="asa1997"
 
-source $HOME/$USR/KOBman/bin/release_var_setup.sh
+#source $HOME/$USR/KOBman/bin/release_var_setup.sh
 
+if [ -z "$KOB_ARCHIVE_DOWNLOAD_REPO" ];
+    then
+        KOB_ARCHIVE_DOWNLOAD_REPO=${KOB_ARCHIVE_DOWNLOAD_REPO:-KOBman_target_repo}
+fi
+echo $KOB_ARCHIVE_DOWNLOAD_REPO
+if  [ -z "$KOB_DIR" ];
+    then
+        KOB_DIR=~/KOBman
+fi
+
+if [ -z "$KOB_NAMESPACE" ];
+    then
+        KOB_NAMESPACE=${KOB_NAMESPACE:-hyperledgerkochi}
+
+fi
+echo $KOB_NAMESPACE
 #Checkout latest tag
 git checkout tags/$kob_rel_version -b $kob_rel_version
 
 echo "making the tar files..."
-tar -cvf $KOBMAN_DIR/bin/kobman-latest.tar $KOBMAN_DIR/src/ $KOBMAN_DIR/bin/
-cp $KOBMAN_DIR/bin/kobman-latest.tar $KOBMAN_DIR/bin/kobman-$kob_rel_version.tar
+tar -cvf $KOB_DIR/bin/kobman-latest.tar $KOB_DIR/src/ $KOB_DIR/bin/
+cp $KOB_DIR/bin/kobman-latest.tar $KOB_DIR/bin/kobman-$kob_rel_version.tar
 
 
 mkdir $kob_rel_stage_area
 cd $kob_rel_stage_area
-git clone $LINK_TO_DOWNLOAD_REPO 
+git clone https://github.com/$KOB_NAMESPACE/$KOB_ARCHIVE_DOWNLOAD_REPO
 mkdir $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
 
 #Moving necessary files to the target repo
-mv $KOBMAN_DIR/bin/kobman*.tar $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
-mv $KOBMAN_DIR/scripts/get.kobman.io $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
-mv $KOBMAN_DIR/scripts/README.md $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
+mv $KOB_DIR/bin/kobman*.tar $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
+mv $KOB_DIR/scripts/get.kobman.io $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
+mv $KOB_DIR/scripts/README.md $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
 
 
 cd $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/
 git pull 
 echo "saving changes and pushing"
-sudo git add .
-sudo git commit -m "Released the version $kob_rel_version"
+git add .
+git commit -m "Released the version $kob_rel_version"
 git push origin master -f
 
-cd $KOBMAN_DIR
-sudo rm -rf $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO $kob_rel_stage_area
+cd $KOB_DIR
+rm -rf $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO $kob_rel_stage_area
 
 git checkout master
 git merge $kob_rel_version
--- bin/release.sh
+++ bin/release.sh
@@ -7,42 +7,50 @@ branch="release"
 #kob_archive_download_repo="KOBman_target_repo"
 # kob_namespace=${KOBMAN_NAMESPACE:-hyperledgerkochi}
 
-source $HOME/$USR/KOBman/bin/release_var_setup.sh
+# source $HOME/$USR/KOBman/bin/release_var_setup.sh
 
-# sanityls
-# if [[ -z "$kob_version" ]]; 
-#     then
-#         echo "Usage: release.sh <version>"
-#  	    exit 0
-# fi
+sanityls
+if [[ -z "$kob_version" ]]; 
+    then
+        echo "Usage: release.sh <version>"
+ 	    exit 0
+fi
 
-# if [ -z $KOB_NAMESPACE ];
-#     then
-#         KOB_NAMESPACE=${KOB_NAMESPACE:-hyperledgerkochi}
+if [ -z "$KOB_NAMESPACE" ];
+    then
+        KOB_NAMESPACE=${KOB_NAMESPACE:-hyperledgerkochi}
 
-# fi
-
-
+fi
+echo $KOB_NAMESPACE
+if [ -z "$KOB_ARCHIVE_DOWNLOAD_REPO" ];
+    then
+        KOB_ARCHIVE_DOWNLOAD_REPO=${KOB_ARCHIVE_DOWNLOAD_REPO:-KOBman_target_repo}
+fi
+echo $KOB_ARCHIVE_DOWNLOAD_REPO
+if  [ -z "$KOB_DIR" ];
+    then
+        KOB_DIR=~/KOBman
+fi
+echo $KOB_DIR
 git checkout master
 
 #copy the tmpl file to /scripts and rename it
-cp $KOBMAN_DIR/scripts/tmpl/get.kobman.io.tmpl $KOBMAN_DIR/scripts/
-mv $KOBMAN_DIR/scripts/get.kobman.io.tmpl $KOBMAN_DIR/scripts/get.kobman.io
-cp $KOBMAN_DIR/scripts/tmpl/README.md.tmpl $KOBMAN_DIR/scripts/
-mv $KOBMAN_DIR/scripts/README.md.tmpl $KOBMAN_DIR/scripts/README.md
+cp $KOB_DIR/scripts/tmpl/get.kobman.io.tmpl $KOB_DIR/scripts/
+mv $KOB_DIR/scripts/get.kobman.io.tmpl $KOB_DIR/scripts/get.kobman.io
+cp $KOB_DIR/scripts/tmpl/README.md.tmpl $KOB_DIR/scripts/
+mv $KOB_DIR/scripts/README.md.tmpl $KOB_DIR/scripts/README.md
 #replacing variables with actual values
-sed -i "s/@KOB_VERSION@/$kob_version/g" $KOBMAN_DIR/scripts/get.kobman.io
-sed -i "s/@KOB_ARCHIVE_DOWNLOAD_REPO@/$KOB_ARCHIVE_DOWNLOAD_REPO/g" $KOBMAN_DIR/scripts/get.kobman.io
-sed -i "s/@KOB_NAMESPACE@/$KOB_NAMESPACE/g" $KOBMAN_DIR/scripts/get.kobman.io
-sed -i "s/@KOB_ARCHIVE_DOWNLOAD_REPO@/$KOB_ARCHIVE_DOWNLOAD_REPO/g" $KOBMAN_DIR/scripts/README.md
-sed -i "s/@KOB_NAMESPACE@/$KOB_NAMESPACE/g" $KOBMAN_DIR/scripts/README.md
+sed -i "s/@KOB_VERSION@/$kob_version/g" $KOB_DIR/scripts/get.kobman.io
+sed -i "s/@KOB_ARCHIVE_DOWNLOAD_REPO@/$KOB_ARCHIVE_DOWNLOAD_REPO/g" $KOB_DIR/scripts/get.kobman.io
+sed -i "s/@KOB_NAMESPACE@/$KOB_NAMESPACE/g" $KOB_DIR/scripts/get.kobman.io
+sed -i "s/@KOB_ARCHIVE_DOWNLOAD_REPO@/$KOB_ARCHIVE_DOWNLOAD_REPO/g" $KOB_DIR/scripts/README.md
+sed -i "s/@KOB_NAMESPACE@/$KOB_NAMESPACE/g" $KOB_DIR/scripts/README.md
 
 git add .
 git commit -m "Variables replaced with originals"
-echo "before exit"
-exit 0
+
 #Tagging and pushing 
-sudo git tag -a "$kob_version" -m "Releasing version $kob_version"
+git tag -a "$kob_version" -m "Releasing version $kob_version"
 git push origin $kob_version
 
 git checkout -b $branch
