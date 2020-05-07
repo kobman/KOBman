bd495a31bff9871399cf1180db727e0d70d9172d
Arun Suresh <arunsureshampadath@gmail.com>
2020-05-07 12:40:28 +0530

    Updated

--- bin/dopublish.sh
+++ bin/dopublish.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 kob_rel_version=$1
-#kob_archive_download_repo="KOBman_target_repo"
-kob_rel_stage_area=~/tmp
-#kob_namespace="asa1997"
-
-#source $HOME/$USR/KOBman/bin/release_var_setup.sh
 
 if [ -z "$KOB_ARCHIVE_DOWNLOAD_REPO" ];
     then
@@ -22,27 +17,27 @@ if [ -z "$KOB_NAMESPACE" ];
         KOB_NAMESPACE=${KOB_NAMESPACE:-hyperledgerkochi}
 
 fi
+
 echo $KOB_NAMESPACE
 #Checkout latest tag
 git checkout tags/$kob_rel_version -b $kob_rel_version
 
-echo "making the tar files..."
-tar -cvf $KOB_DIR/bin/kobman-latest.tar $KOB_DIR/src/ $KOB_DIR/bin/
-cp $KOB_DIR/bin/kobman-latest.tar $KOB_DIR/bin/kobman-$kob_rel_version.tar
 
-
-mkdir $kob_rel_stage_area
-cd $kob_rel_stage_area
+mkdir $KOB_DIR/tmp
+cd $KOB_DIR/tmp
 git clone https://github.com/$KOB_NAMESPACE/$KOB_ARCHIVE_DOWNLOAD_REPO
-mkdir $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
+mkdir $KOB_DIR/tmp/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
+
+echo "making the tar files..."
+tar -cvf $KOB_DIR/tmp/$KOB_ARCHIVE_DOWNLOAD_REPO/dist/kobman-latest.tar $KOB_DIR/src/ $KOB_DIR/bin/
+cp $KOB_DIR/tmp/$KOB_ARCHIVE_DOWNLOAD_REPO/dist/kobman-latest.tar $KOB_DIR/bin/kobman-$kob_rel_version.tar
+
 
 #Moving necessary files to the target repo
-mv $KOB_DIR/bin/kobman*.tar $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
-mv $KOB_DIR/scripts/get.kobman.io $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
-mv $KOB_DIR/scripts/README.md $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
+mv $KOB_DIR/scripts/* $KOB_DIR/tmp/$KOB_ARCHIVE_DOWNLOAD_REPO/dist
 
 
-cd $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO/
+cd $KOB_DIR/tmp/$KOB_ARCHIVE_DOWNLOAD_REPO/
 git pull 
 echo "saving changes and pushing"
 git add .
@@ -50,10 +45,11 @@ git commit -m "Released the version $kob_rel_version"
 git push origin master -f
 
 cd $KOB_DIR
-rm -rf $kob_rel_stage_area/$KOB_ARCHIVE_DOWNLOAD_REPO $kob_rel_stage_area
+rm -rf tmp/$KOB_ARCHIVE_DOWNLOAD_REPO tmp/
 
 git checkout master
-git merge $kob_rel_version
+
 git branch -D $kob_rel_version
+
 
 
