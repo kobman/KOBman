7ed06a62cacd8f4f406c849b9a7291af0d843263
EtricKombat <ericjoy1991@gmail.com>
2020-05-18 22:59:34 +0530

    kobman-install modularization commit on: Mon May 18 22:59:34 IST 2020

--- src/main/bash/commands/kobman-install.sh
+++ src/main/bash/commands/kobman-install.sh
@@ -1,31 +1,6 @@
 #!/usr/bin/env bash
 
 
-#2) __kobman_identify_input_argument()
-	# read the arguments, identify each argument and assign it to variable using if else logic or case statement
-	# for the environment name variable call _validate_and_set_environment()
-	# for the version variable call _Validate_and_set_version()
-	# for the namespace variable call _set_namespace()
-	# for default case (no namespace, version is passed by user as argument) call _set_default()
-
-#5) __kobman_set_namespace()
-	# assigning the argument value to the namespace variable
-
-#6) __kobman_set_default()
-	# set the version to the latest version and namespace to the hyperledgerkochi (environment variable value)
-	# check if the environment variable has a value, version has a value and namespace has a value then call _create_environment_directory()
-
-
-#7) __kobman_create_environment_directory()
-	# create the dir in the specified location
-	# create the current file with the latest verson identify
-	# call _confirm_environment_dir_exist
-
-#8) __kobman_confirm_environment_dir_exist()
-
-
-
-
 function __kob_install {
 
 #Latest version check and assignment should happen here (if latest has been released )
@@ -115,7 +90,6 @@ function __kobman_validate_and_set_version
         	if [ "$?" -eq "0" ];    # check version.txt is empty or not (or version variable is empty or not)
         	then
                 	version_value=$1
-			echo "Valid Version"	
         		return 0	
 		else
                 	__kobman_echo_red "version not available"
@@ -131,12 +105,14 @@ function __kobman_validate_and_set_version
 
 function __kobman_validate_version_format
 {
-	__kobman_echo_no_colour "$1" | grep -w '[0-9]*\.[0-9]*\.[0-9]*' 
+	__kobman_echo_no_colour "$1" | grep -w '[0-9]*\.[0-9]*\.[0-9]*' > /dev/null
+
 }
 
 function __kobman_check_and_confirm_existing_version
 {
-       	git ls-remote --tags "https://github.com/${1}/KOBman" | grep -w 'refs/tags/[0-9]*\.[0-9]*\.[0-9]*' | sort -r | head | grep -o '[^\/]*$' | grep -w "$2" 
+       	git ls-remote --tags "https://github.com/${1}/KOBman" | grep -w 'refs/tags/[0-9]*\.[0-9]*\.[0-9]*' | sort -r | head | grep -o '[^\/]*$' | grep -w "$2" > /dev/null
+
 }
 
 
