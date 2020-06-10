5a803c339a33b06b70959d97eccccbe59c4064ca
Arun Suresh <arunsureshampadath@gmail.com>
2020-06-10 22:59:04 +0530

    Added script for list command

--- src/main/bash/commands/kobman-list.sh
+++ src/main/bash/commands/kobman-list.sh
@@ -2,6 +2,18 @@
 
 function __kob_list {
 
-curl -L "${KOBMAN_SERVICE}/${KOBMAN_NAMESPACE}/KOBman/master/dist/list" | less
+__kobman_secure_curl "https://raw.githubusercontent.com/$KOBMAN_NAMESPACE/KOBman/master/dist/list.txt" > tmp.txt
 
+# envs="Von-Network,TheOrgBook,greenlight,kobman,KOBConnect,KOBRegistry,KochiOrgBook,KOBDflow,KOBVON"
+__kobman_echo_white "Available environments and their respective version numbers"
+__kobman_echo_white "---------------------------------------------------------------"
+# for i in $envs; do
+#     cat tmp.txt | grep "$i" >> tmp1.txt
+# done
+# cat tmp1.txt
+sed 's/,/ /g' tmp.txt > tmp1.txt
+sed 's/,/ /g' tmp.txt | cut -d " " -f 1 > tmp2.txt
+cat tmp1.txt | grep -h -f tmp2.txt > tmp3.txt
+cat tmp3.txt
+rm tmp*
 }
