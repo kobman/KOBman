52ba6d8fe4d1b25caa2a9dd0603eda759e7c1f22
Arun Suresh <arunsureshampadath@gmail.com>
2020-06-16 21:34:25 +0530

    Added dummyenv files; Need to add uninstall for dummyenv

--- tests/commands/test-kob-status.sh
+++ tests/commands/test-kob-status.sh
@@ -18,25 +18,43 @@ function __test_kob_init
         
     fi
     
-    __kobman_echo_no_colour "Checking for installed environments...."
+    # __kobman_echo_no_colour "Checking for installed environments...."
     env_folder=($(find $KOBMAN_DIR/envs/ -name "kob_env_*" -print))
-    if [[ -z $env_folder ]]; then
-        __kobman_echo_no_colour "No environments installed"
-        __kobman_echo_no_colour "Please install an enivornment first and try again"
-        __kobman_echo_no_colour "Exiting!!!"
-        status="false"
-        return 1
-    else
-        __kobman_echo_no_colour "Environments found"
-        __kobman_echo_no_colour "Proceeding with the test..."
-    fi
-
+    # if [[ -z $env_folder ]]; then
+    #     __kobman_echo_no_colour "No environments installed"
+    #     __kobman_echo_no_colour "Please install an enivornment first and try again"
+    #     __kobman_echo_no_colour "Exiting!!!"
+    #     status="false"
+    #     return 1
+    # else
+    #     __kobman_echo_no_colour "Environments found"
+    #     __kobman_echo_no_colour "Proceeding with the test..."
+    # fi
+    __kobman_echo_no_colour "creating and sourcing dummyenv files..."
+    touch $KOBMAN_DIR/var/kobman_env_$kobman_env_name.proc
+  
+    fake_publish_dummyenv
+  
+    create_install_dummyenv_script > $path_to_kob_envs/kobman-$kobman_env_name.sh
+    source $KOBMAN_DIR/bin/kobman-init.sh
 }
 
 function __test_kob_execute
 {
-    __kobman_echo_no_colour "Executing status command"
-    kob status >> tmp.txt
+    kob status >> state1.txt
+    __kobman_echo_no_colour "Executing install command for various versions of dummyenv"
+    __kobman_echo_white "Version-0.0.2"
+    kob install --environment dummyenv --version 0.0.2
+    kob status >> state2.txt
+    cat state2.txt
+    __kobman_echo_white "Version-0.0.3"
+    kob install --environment --version 0.0.3
+    kob status >> state3.txt
+    cat state3.txt
+    __kobman_echo_no_colour "Uninstalling dummyenv - version 0.0.2"
+    __kobman_uninstall_dummyenv 0.0.2
+    kob status >> state4.txt
+    cat state4.txt
 
 }
 
@@ -72,6 +90,59 @@ function __test_kob_validate
 function __test_kob_cleanup
 {
     rm tmp.txt
+}
+
+function create_install_dummyenv_script(){
+  
+cat <<EOF
+#!/bin/bash
+function __kobman_install_dummyenv
+{
+  
+  cd ~
+  if [ ! -d "Dev_dummyenv" ]; then 
+    
+    __kobman_create_dev_environment 
+    __kobman_echo_white "dummyenv installed"
+  else
+    
+    rm -rf ~/Dev_dummyenv	
+    __kobman_create_dev_environment  
+    __kobman_echo_white "dummyenv installed"
+    
+  fi		
+}
+function __kobman_uninstall_dummyenv
+{
+    version=$1
+    rm -rf $KOBMAN_DIR/envs/kob_env_dummyenv/$version
+}
+function __kobman_update_dummyenv
+{
+    __kobman_echo_white "update"
+}
+function __kobman_upgrade_dummyenv
+{
+    __kobman_echo_white "upgraded"
+}
+function __kobman_start_dummyenv
+{
+    __kobman_echo_white "start"
+}
+function __kobman_stop_dummyenv
+{
+    __kobman_echo_white "stop"
+}
+function __kobman_create_dev_environment 
+{
+	 
+	cd ~
+	mkdir -p Dev_dummyenv
+	cd Dev_dummyenv
+	export DUMMYENV_DEV_DIR=$PWD
+	mkdir -p test/ dependency/
+}
+EOF
 }
 
 function __test_kob_run
