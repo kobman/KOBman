c34a44ad8c4bba71b1c867231956dbff27f1545f
EtricKombat <ericjoy1991@gmail.com>
2020-06-17 09:54:00 +0530

    RF-1.0.0.3.1 - KOBman version - variable name changing to meaningful

--- src/main/bash/commands/kobman-version.sh
+++ src/main/bash/commands/kobman-version.sh
@@ -1,73 +1,33 @@
 #!/usr/bin/env bash
 
 
+
 function __kob_version {
 
- 	local command deployment_type environment_type name_space 
-	command=$COMMAND
-	deployment_type=$DEPLOYMENT_TYPE
-	environment_type=$ENVIRONMENT_TYPE
-	name_space=$NAME_SPACE                                                                   
-	if [ "$DEPLOYMENT_TYPE" == "" ] 
-	then
-		cd "${KOBMAN_DIR}/var"	
-		cat version.txt	
-		cd ~	
-	else	
-		__kobman_environment_version  
-	fi
+        local environment_type environment_name
+
+        environment_type=$ENVIRONMENT_TYPE
+        environment_name=$NAME_SPACE
+        if [ "$DEPLOYMENT_TYPE" == "" ]
+        then
+                cd "${KOBMAN_DIR}/var"
+                cat version.txt
+                cd ~
+        else
+                __kobman_environment_version "$environment_type" "$environment_name"
+        fi
 }
 
 
 
 function __kobman_environment_version {
 
- 	local name_space 
-	name_space=$1
-	if [ "$DEPLOYMENT_TYPE" == "--environment" ] 
-	then	
-	
-		case "$ENVIRONMENT_TYPE" in
-		tobvon)
-			__kobman_version_tobvon 
+        local environment_type environment_name
+        environment_type=$1
+        environment_name=$2
+        if [ "$environment_type" == "--environment" ]
+        then
 
-		;;	
-		tob)
-			__kobman_version_tob 
-
-		;;	
-		greenlight)
-			__kobman_version_greenlight 
-		;;	
-		kobman)
-			__kobman_version_kobman 
-
-		;;	
-		kobvon)
-			echo "invoking kobman version"
-			__kobman_version_kobvon 
-
-		;;	
-		kob)
-			__kobman_version_kob 
-
-		;;	
-		kobdflow)
-			__kobman_version_kobdflow 
-
-		;;	
-		kobconnect)
-			__kobman_version_kobconnect 
-
-		;;	
-		kobregistery)
-			__kobman_version_kobregistery
-
-		;;
-		"")
-			echo "verify your command &  try again"	
-
-		;;	
-	esac
-	fi
+                __kobman_version_${environment_name}
+        fi
 }
