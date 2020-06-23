8cafea7529a87e5711646dc28fc280e591596571
Arun Suresh <arunsureshampadath@gmail.com>
2020-06-23 16:33:20 +0530

    WIP

--- tests/commands/test-kob-uninstall.sh
+++ tests/commands/test-kob-uninstall.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
 
+path_to_kob_envs="${KOBMAN_DIR}/envs"
+path_to_kob_env_tests=~/KOBman/tests/envs
+environment=$1
+version=$2
+if [[ -z $environment || -z $version ]]; then
+    echo "Usage: ./test-kob-uninstall.sh <env_name> <version>"
+fi
+]
 
 
