7226b2d1b8516188104f078fb1f6314757781014
Arun Suresh <arunsureshampadath@gmail.com>
2020-06-29 15:11:27 +0530

    Added script for upgrade

--- src/main/bash/commands/kobman-upgrade.sh
+++ src/main/bash/commands/kobman-upgrade.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 function __kob_upgrade {
+mkdir ~/user_configs
+zip -r user_configs/kobman_backup.zip $KOBMAN_DIR
+__kobman_secure_curl "https://raw.githubusercontent.com/hyperledgerkochi/KOBman/dist/dist/get.kobman.io | bash"
+unzip user_configs/kobman_backup.zip -d ~/user_configs
+if [[ -d ~/user_configs/.kobman/envs/kob_env_* ]]; then
+    mv ~/user_configs/.kobman/envs/kob_env_* $KOBMAN_DIR/envs/
+fi
+if [[ -f ~/user_configs/.kobman/var/*.proc ]]; then
+    mv ~/user_configs/.kobman/var/*.proc $KOBMAN_DIR/var/
+fi
+if [[ -f ~/user_configs/.kobman/var/current ]]; then
+    mv ~/user_configs/.kobman/var/current $KOBMAN_DIR/var/
+fi
 
-__kobman_echo_red "Testing upgrade Command"
+__kobman_echo_blue "Upgraded successfully"
+__kobman_echo_blue "Current version:$(cat $KOBMAN_DIR/var/version.txt)"
 
+rm -rf ~/user_configs
 
+##TODO:- validate whether the user configs are compatible with the current version
 }
