48f777b8cf53fd5d416a6587e8a9c67f2f8344ea
Arun Suresh <arunsureshampadath@gmail.com>
2020-06-30 10:26:48 +0530

    Updated upgrade command; Added missing remove command

--- src/main/bash/commands/kobman-upgrade.sh
+++ src/main/bash/commands/kobman-upgrade.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
 function __kob_upgrade {
 mkdir ~/user_configs
-zip -r user_configs/kobman_backup.zip $KOBMAN_DIR
-__kobman_secure_curl "https://raw.githubusercontent.com/hyperledgerkochi/KOBman/dist/dist/get.kobman.io | bash"
+__kobman_echo_white "Making backups..."
+zip -r user_configs/kobman_backup.zip .kobman
+__kobman_echo_white "Removing current version..."
+rm -rf $KOBMAN_DIR
+__kobman_echo_white "Fetching latest version..."
+__kobman_secure_curl https://raw.githubusercontent.com/EtricKombat/KOBman/dist/dist/get.kobman.io | bash
 unzip user_configs/kobman_backup.zip -d ~/user_configs
-if [[ -d ~/user_configs/.kobman/envs/kob_env_* ]]; then
-    mv ~/user_configs/.kobman/envs/kob_env_* $KOBMAN_DIR/envs/
+__kobman_echo_white "Restoring user configs..."
+dir=$(find  ~/user_configs/.kobman/envs -type d -name kob_env_*)
+if [[ -n $dir ]]; then
+    mv ~/user_configs/.kobman/envs/kob_env_* $KOBMAN_DIR/envs
 fi
 if [[ -f ~/user_configs/.kobman/var/*.proc ]]; then
     mv ~/user_configs/.kobman/var/*.proc $KOBMAN_DIR/var/
@@ -14,7 +19,7 @@ fi
 if [[ -f ~/user_configs/.kobman/var/current ]]; then
     mv ~/user_configs/.kobman/var/current $KOBMAN_DIR/var/
 fi
-
+source $KOBMAN_DIR/bin/kobman-init.sh
 __kobman_echo_blue "Upgraded successfully"
 __kobman_echo_blue "Current version:$(cat $KOBMAN_DIR/var/version.txt)"
 
@@ -22,3 +27,6 @@ rm -rf ~/user_configs
 
 ##TODO:- validate whether the user configs are compatible with the current version
 }
+
+
+
