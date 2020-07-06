80a1a2194bb6170f4c1cf982420c0ba88a84ff57
Arun Suresh <arunsureshampadath@gmail.com>
2020-07-06 14:06:17 +0530

    hardcoding removed; ~ changed to $HOME

--- src/main/bash/envs/kobman-KOBman.sh
+++ src/main/bash/envs/kobman-KOBman.sh
@@ -3,23 +3,24 @@
 function __kobman_install_KOBman
 {
 	local environment_name="$1"
+	local dev_area_name="Dev_$environment_name"
 	local version_id="$2"
 
 	
-	if [[ ! -d ~/Dev_KOBman ]]; then
+	if [[ ! -d $HOME/$dev_area_name ]]; then
 		__kobman_create_fork "${environment_name}" || return 1
  		__kobman_echo_white "Creating Dev environment for ${environment_name}"
  		__kobman_echo_white "from https://github.com/${KOBMAN_NAMESPACE}/${environment_name}"
  		__kobman_echo_white "version :${version_id} "
-		__kobman_create_dev_environment "${environment_name}"
+		__kobman_create_dev_environment "${dev_area_name}" "$environment_name"
 		__kobman_echo_violet "Dev environment for ${environment_name} created successfully"
 	else
  		__kobman_echo_white "Removing existing version "
-		rm -rf ~/Dev_${environment_name}
+		rm -rf $HOME/$dev_area_name
  		__kobman_echo_white "Creating Dev environment for ${environment_name}"
  		__kobman_echo_white "from https://github.com/${KOBMAN_NAMESPACE}/${environment_name}"
  		__kobman_echo_white "version :${version_id} "
-		__kobman_create_dev_environment "${environment_name}"
+		__kobman_create_dev_environment "${dev_area_name}"
 		__kobman_echo_violet "Dev environment for ${environment_name} created successfully"
 	fi
 
@@ -29,16 +30,18 @@ function __kobman_install_KOBman
 
 function __kobman_create_dev_environment 
 {
-	local environment="$1"
-	mkdir -p ~/Dev_$environment
-	git clone -q https://github.com/$KOBMAN_NAMESPACE/${environment_name} ~/Dev_$environment/$environment
-	export Dev_${environment}="~/Dev_${environment}"
-	mkdir -p ~/Dev_"${environment}"/test
-	mkdir -p ~/Dev_"${environment}"/dependency
+	local dev_area_name="$1"
+	local environment_name=$2
+	mkdir -p $HOME/$dev_area_name
+	git clone -q https://github.com/$KOBMAN_NAMESPACE/${environment_name} $HOME/$dev_area_name/$environment_name
+	export ${dev_area_name}="$HOME/${dev_area_name}"
+	mkdir -p $HOME/"${dev_area_name}"/test
+	mkdir -p $HOME/"${dev_area_name}"/dependency
 }
 
 function __kobman_uninstall_KOBman
 {
+	local dev_area_name="Dev_$1"
 	__kobman_echo_white "Removing dev environment for KOBman"
-	rm -rf ~/Dev_KOBman
+	rm -rf $HOME/$dev_area_name
 }
