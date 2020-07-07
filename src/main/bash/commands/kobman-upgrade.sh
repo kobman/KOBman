1dfa77809f3fac564cee9d42513ac4fdccfa34df
Arun Suresh <arunsureshampadath@gmail.com>
2020-07-07 22:22:09 +0530

    Updated for testing

--- src/main/bash/commands/kobman-upgrade.sh
+++ src/main/bash/commands/kobman-upgrade.sh
@@ -2,16 +2,16 @@
 function __kob_upgrade {
 mkdir $KOBMAN_DIR/bak
 __kobman_echo_white "Making backups..."
-zip -r $KOBMAN_DIR/bak/kobman_backup.zip .kobman
+zip -r $KOBMAN_DIR/bak/kobman_bak.zip .kobman
 __kobman_echo_white "Removing current version..."
-find $KOBMAN_DIR -mindepth 1 -name backup -prune -o -exec rm -rf {} +
+find $KOBMAN_DIR -mindepth 1 -name bak -prune -o -exec rm -rf {} +
 __kobman_echo_white "Fetching latest version..."
 __kobman_secure_curl https://raw.githubusercontent.com/$KOBMAN_NAMESPACE/KOBman/dist/dist/get.kobman.io | bash
-unzip $KOBMAN_DIR/bak/kobman_backup.zip -d $KOBMAN_DIR/bak
+unzip $KOBMAN_DIR/bak/kobman_bak.zip -d $KOBMAN_DIR/bak
 __kobman_echo_white "Restoring user configs..."
-dir=$(find  $KOBMAN_DIR/bak/.kobman/envs -type d -name kob_env_*)
+dir=$(find  $KOBMAN_DIR/bak/.kobman/envs -type d -name kobman-*)
 if [[ -n $dir ]]; then
-    mv $KOBMAN_DIR/bak/.kobman/envs/kob_env_* $KOBMAN_DIR/envs
+    mv $KOBMAN_DIR/bak/.kobman/envs/kobman-* $KOBMAN_DIR/envs
 fi
 if [[ -f $KOBMAN_DIR/bak/.kobman/var/*.proc ]]; then
     mv $KOBMAN_DIR/bak/.kobman/var/*.proc $KOBMAN_DIR/var/
