6d5a125ede714a4be5fcefeb95b56fb525e9906e
Arun Suresh <arunsureshampadath@gmail.com>
2020-07-08 17:04:15 +0530

    WIP

--- tests/commands/test-kob-upgrade.sh
+++ tests/commands/test-kob-upgrade.sh
@@ -5,23 +5,51 @@ function __test_kob_init
         echo "kobman not found"
         echo "Please install kobman first and try again"
         exit 1
+    else
+        source $KOBMAN_DIR/bin/kobman-init.sh
+        source $KOBMAN_DIR/src/kobman-utils.sh
+        __kobman_echo_no_colour "kob found"
     fi
 
+    if [[ -z $FAKE_REPO_NAMESPACE ]]; then
+        __kobman_echo_no_colour "No dummy namespace found"
+        __kobman_echo_no_colour "Cannot run test script without fake repository"
+        __kobman_echo_no_colour "Exiting!!!"
+        exit 1
+    fi
 }
 
 function __test_kob_execute
 {
+    __kobman_echo_no_colour "Executing commands"
+    kob -V >> $HOME/old.txt
+    tree $KOBMAN_DIR/envs >> $HOME/tree1.txt
     kob upgrade >> $HOME/output.txt
+    tree $KOBMAN_DIR/envs >> $HOME/tree2.txt
+    kob -V >> $HOME/new.txt
 }
 
 function __test_kob_validate
 {
+    __kobman_echo_no_colour "Validating upgrade command...."
+    diff1=$(comm -3 $HOME/old.txt $HOME/new.txt)
+    if [[ -z $diff1 ]]; then
+        __kobman_echo_no_colour "Version did not change after upgrading"
+        test_status="failed"
+        return 1
+    fi
 
+    diff2=$(comm -3 $HOME/tree1.txt $HOME/tree2.txt)
+    if [[ -n $diff2 ]]; then
+        __kobman_echo_no_colour "User configs were not restored properly"
+        test_status="failed"
+        return 1
+    fi
 }
 
 function __test_kob_cleanup
 {
-    rm $HOME/output.txt
+    rm $HOME/output.txt $HOME/old.txt $HOME/new.txt $HOME/tree1.txt $HOME/tree2.txt
 }
 function __test_kob_run
 {
@@ -30,4 +58,10 @@ function __test_kob_run
     __test_kob_execute
     __test_kob_validate
     __test_kob_cleanup
+    if [[ $test_status == "success" ]]; then
+        __kobman_echo_green "test-kob-update success"
+    else
+        __kobman_echo_red "test-kob-update failed"
+    fi
 }
+__test_kob_run
