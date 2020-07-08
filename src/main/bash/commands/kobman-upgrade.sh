3a4a5bddde42d294e8b1dcb07c92958cb802c19d
Arun Suresh <arunsureshampadath@gmail.com>
2020-07-08 17:36:39 +0530

    kob_env_ >> kobman-; backup >> bak

--- src/main/bash/commands/kobman-upgrade.sh
+++ src/main/bash/commands/kobman-upgrade.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 function __kob_upgrade {
-mkdir $KOBMAN_DIR/backup
+mkdir $KOBMAN_DIR/bak
 __kobman_echo_white "Making backups..."
-zip -r $KOBMAN_DIR/backup/kobman_backup.zip .kobman
+zip -r $KOBMAN_DIR/bak/kobman_bak.zip .kobman
 __kobman_echo_white "Removing current version..."
-find $KOBMAN_DIR -mindepth 1 -name backup -prune -o -exec rm -rf {} +
+find $KOBMAN_DIR -mindepth 1 -name bak -prune -o -exec rm -rf {} +
 __kobman_echo_white "Fetching latest version..."
 __kobman_secure_curl https://raw.githubusercontent.com/$KOBMAN_NAMESPACE/KOBman/dist/dist/get.kobman.io | bash
-unzip $KOBMAN_DIR/backup/kobman_backup.zip -d $KOBMAN_DIR/backup
+unzip $KOBMAN_DIR/bak/kobman_bak.zip -d $KOBMAN_DIR/bak
 __kobman_echo_white "Restoring user configs..."
-dir=$(find  $KOBMAN_DIR/backup/.kobman/envs -type d -name kob_env_*)
+dir=$(find  $KOBMAN_DIR/bak/.kobman/envs -type d -name kobman-*)
 if [[ -n $dir ]]; then
-    mv $KOBMAN_DIR/backup/.kobman/envs/kob_env_* $KOBMAN_DIR/envs
+    mv $KOBMAN_DIR/bak/.kobman/envs/kobman-* $KOBMAN_DIR/envs
 fi
-if [[ -f $KOBMAN_DIR/backup/.kobman/var/*.proc ]]; then
-    mv $KOBMAN_DIR/backup/.kobman/var/*.proc $KOBMAN_DIR/var/
+if [[ -f $KOBMAN_DIR/bak/.kobman/var/*.proc ]]; then
+    mv $KOBMAN_DIR/bak/.kobman/var/*.proc $KOBMAN_DIR/var/
 fi
-if [[ -f $KOBMAN_DIR/backup/.kobman/var/current ]]; then
-    mv $KOBMAN_DIR/backup/.kobman/var/current $KOBMAN_DIR/var/
+if [[ -f $KOBMAN_DIR/bak/.kobman/var/current ]]; then
+    mv $KOBMAN_DIR/bak/.kobman/var/current $KOBMAN_DIR/var/
 fi
 source $KOBMAN_DIR/bin/kobman-init.sh
 __kobman_echo_blue "Upgraded successfully"
 __kobman_echo_blue "Current version:$(cat $KOBMAN_DIR/var/version.txt)"
 
-rm -rf $KOBMAN_DIR/backup
+rm -rf $KOBMAN_DIR/bak
 
 ##TODO:- validate whether the user configs are compatible with the current version
 }
