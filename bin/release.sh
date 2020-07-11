0cd8a91414de4e17c11cd7dca2d8275b8e764698
Arun Suresh <arunsureshampadath@gmail.com>
2020-07-11 19:34:43 +0530

    ssh_install >> master

--- bin/release.sh
+++ bin/release.sh
@@ -24,7 +24,7 @@ fi
 
 # prepare branch
 cd ~/KOBman
-git checkout ssh_install
+git checkout master
 git branch -D $branch
 git checkout -b $branch
 
@@ -53,4 +53,4 @@ git tag -a $kob_version -m "Releasing version $kob_version"
 git push origin $kob_version
 
 #checkout to dev
-git checkout ssh_install
+git checkout master
