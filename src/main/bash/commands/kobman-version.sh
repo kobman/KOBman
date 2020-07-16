2e3503ea096c616cbd765cf1e8333fa34e9af18c
asa1997 <arunsureshampadath@gmail.com>
2020-07-16 10:23:20 +0530

    added version

--- src/main/bash/commands/kobman-version.sh
+++ src/main/bash/commands/kobman-version.sh
@@ -1,16 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+ 
 
 function __kob_version {
+
+ 
 
 
   local environment_parameter=$1
   local environment_value=$2
 
+ 
+
   if [ -z "$environment_parameter" ]
-	then
-		echo "KOBman version" "$(cat ${KOBMAN_DIR}/var/version.txt)"
-	else
-		__kobman_version_"$environment_value"
-	fi
+        then
+                echo "KOBman utility version" "$(cat ${KOBMAN_DIR}/var/version.txt)"
+    return 0
+        fi
+
+ 
+
+  if [[ ! -z $environment_value && -f "${KOBMAN_DIR}/envs/kob_env_${environment_value}/current" ]]
+  then
+                echo "${environment_value} version" "$(cat ${KOBMAN_DIR}/envs/kob_env_${environment_value}/current)"
+  else
+                __kobman_echo_red "$environment_value environment is not installed in the Local system !"
+  fi
+
+ 
+
 }
