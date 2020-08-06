544f14832ef2735e98516f6ffc53398f451c69d3
asa1997 <arunsureshampadath@gmail.com>
2020-08-06 22:58:30 +0530

    master >> user_configs

--- bin/release.sh
+++ bin/release.sh
@@ -24,7 +24,7 @@ fi
 
 # prepare branch
 cd ~/KOBman
-git checkout master
+git checkout user_configs
 git branch -D $branch
 git checkout -b $branch
 
@@ -53,4 +53,4 @@ git tag -a $kob_version -m "Releasing version $kob_version"
 git push origin $kob_version
 
 #checkout to dev
-git checkout master
+git checkout user_configs
