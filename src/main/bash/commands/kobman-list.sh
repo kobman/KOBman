9f459405cf9f59098254d0d319908353c25f2c4c
asa1997 <arunsureshampadath@gmail.com>
2020-08-13 15:36:53 +0530

    Updated code for kob list

--- src/main/bash/commands/kobman-list.sh
+++ src/main/bash/commands/kobman-list.sh
@@ -2,12 +2,10 @@
 
 function __kob_list {
 
-cat $KOBMAN_DIR/var/list.txt > tmp.txt
+# cat $KOBMAN_DIR/var/list.txt > tmp.txt
 __kobman_echo_no_colour "Available environments and their respective version numbers"
 __kobman_echo_no_colour "---------------------------------------------------------------"
-sed 's/,/ /g' tmp.txt > tmp1.txt
-sed 's/,/ /g' tmp.txt | cut -d " " -f 1 > tmp2.txt
-cat tmp1.txt | grep -h -f tmp2.txt > tmp3.txt
-cat tmp3.txt
-rm tmp*.txt
+sed 's/,/ - /1' $KOBMAN_DIR/var/list.txt
+__kobman_echo_no_colour ""
+# rm tmp*.txt
 }
