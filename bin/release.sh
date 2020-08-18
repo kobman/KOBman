f0b7b48563858014f92eb9389af6a3cfd24bd6d1
asa1997 <arunsureshampadath@gmail.com>
2020-08-18 19:36:12 +0530

    refactor_main >> master

--- bin/release.sh
+++ bin/release.sh
@@ -24,7 +24,7 @@ fi
 
 # prepare branch
 cd ~/KOBman
-git checkout refactor_main
+git checkout master
 git branch -D $branch
 git checkout -b $branch
 
@@ -52,5 +52,5 @@ git push -f -u origin $branch
 git tag -a $kob_version -m "Releasing version $kob_version"
 git push origin $kob_version
 
-#checkout to refactor_main
-git checkout refactor_main
+#checkout to master
+git checkout master
