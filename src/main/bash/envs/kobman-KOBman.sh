78882b25658531f42f67d37af7a6c5d733ceba1c
asa1997 <arunsureshampadath@gmail.com>
2020-08-19 23:14:34 +0530

    Updated validate function for KOBman dev

--- src/main/bash/envs/kobman-KOBman.sh
+++ src/main/bash/envs/kobman-KOBman.sh
@@ -11,18 +11,12 @@ function __kobman_install_KOBman
 	
 	if [[ ! -d $KOBMAN_ENV_ROOT ]]; then
 		__kobman_create_fork "${environment_name}" || return 1
- 		__kobman_echo_white "Creating Dev environment for ${environment_name} under $KOBMAN_ENV_ROOT/$environment_name"
- 		__kobman_echo_white "from https://github.com/${KOBMAN_USER_NAMESPACE}/${environment_name}"
- 		__kobman_echo_white "version :${version_id} "
-		__kobman_create_dev_environment "$environment_name" || return 1
+ 		__kobman_create_dev_environment "$environment_name" "$version_id" || return 1
 		__kobman_echo_violet "Dev environment for ${environment_name} created successfully"
 	else
  		__kobman_echo_white "Removing existing version "
 		rm -rf $KOBMAN_ENV_ROOT
- 		__kobman_echo_white "Creating Dev environment for ${environment_name}under $KOBMAN_ENV_ROOT/$environment_name"
- 		__kobman_echo_white "from https://github.com/${KOBMAN_USER_NAMESPACE}/${environment_name}"
- 		__kobman_echo_white "version :${version_id} "
-		__kobman_create_dev_environment  "$environment_name" || return 1
+		__kobman_create_dev_environment  "$environment_name" "$version_id" || return 1
 		__kobman_echo_violet "Dev environment for ${environment_name} created successfully"
 	fi
 
@@ -34,6 +28,10 @@ function __kobman_create_dev_environment
 {
 	
 	local environment_name=$1
+	local version_id=$2
+	__kobman_echo_white "Creating Dev environment for ${environment_name} under $KOBMAN_ENV_ROOT/$environment_name"
+	__kobman_echo_white "from https://github.com/${KOBMAN_USER_NAMESPACE}/${environment_name}"
+	__kobman_echo_white "version :${version_id} "
 	mkdir -p $KOBMAN_ENV_ROOT
 	git clone -q https://github.com/$KOBMAN_USER_NAMESPACE/${environment_name} $KOBMAN_ENV_ROOT/$environment_name
 	if [[ ! -d $KOBMAN_ENV_ROOT || ! -d $KOBMAN_ENV_ROOT/$environment_name ]]; then
@@ -62,40 +60,21 @@ function __kobman_uninstall_KOBman
 	else
 		rm -rf $KOBMAN_ENV_ROOT
 	fi
+	unset KOBMAN_ENV_ROOT
+
 }
 
 function __kobman_validate_KOBman
 {
 	local environment=$1
-	local version=$2
-	local var1="$environment dependency"
-	local var2="kobman-$environment kobman-$environment/$version kobman-$environment/$version/kobman-$environment.sh"
-	for i in "${var1}"; do
-		cat $HOME/ts2_result.out | grep "$KOBMAN_ENV_ROOT/$i"
-		if [[ "$?" != "0" ]]; then
-			__kobman_echo_no_colour "Could not find $KOBMAN_ENV_ROOT/$i"
-			return 1
-		fi
-	done
-	for j in "$var2"; do
-		cat $HOME/ts3_result.out | grep "$KOBMAN_DIR/envs/$i"
-		if [[ "$?" != "0" ]]; then
-			__kobman_echo_no_colour "Could not find $KOBMAN_DIR/envs/$i"
-			return 1
-		fi
-	done
-
-	##CHECKING AFTER UNINSTALLING
-	if [[ -d $KOBMAN_ENV_ROOT ]]; then
-		__kobman_echo_no_colour "Found $KOBMAN_ENV_ROOT even after uninstalling"
+	if [[ ! -d $KOBMAN_ENV_ROOT/$environment ]]; then
+		__kobman_echo_no_colour "Could not find $KOBMAN_ENV_ROOT/$environment"
 		return 1
 	fi
 
-	if [[ -d $KOBMAN_DIR/envs/kobman-$environment ]]; then
-		__kobman_echo_no_colour "Found $KOBMAN_DIR/envs/kobman-$environment even after uninstalling"
-		return 1
-	fi
-	unset environment version var1 var2
+	[[ ! -d $KOBMAN_ENV_ROOT/dependency ]] && __kobman_echo_no_colour "Could not find $KOBMAN_ENV_ROOT/dependency" && return 1
+
+	
 
 }
 
