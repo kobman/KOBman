40bae2fb8b7f735db9bce0626e3323b653bb069e
asa1997 <arunsureshampadath@gmail.com>
2020-10-30 14:10:03 +0530

    removed services

--- src/services/kob-email-verification/src/assets/js/verify.js
+++
@@ -1,6 +0,0 @@
-var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
-var Android = !!navigator.platform && /Android|Linux|Linux armv6l|Linux armv7l/.test(navigator.platform);
-
-if (iOS || Android) {
-  window.location = __didcomm_url;
-}
